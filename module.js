86ef7bbb3e76d081af25a3ff829620fd1a405e53
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-11-11 15:32:15 +0100

    Add files via upload

---
+++ module.js
@@ -0,0 +1,22 @@
+module.exports = {
+modul: {
+	axios: require('axios'),
+	boom: require('@hapi/boom'),
+	baileys: require('@whiskeysockets/baileys'), 
+	chalk: require('chalk'),
+	cheerio: require('cheerio'),
+	child_process: require('child_process'),
+	util: require('util'),
+    Utils: require('@whiskeysockets/baileys/lib/Utils'),
+	fs: require('fs'),
+	fetch: require('node-fetch'),
+	FormData: require('form-data'),
+	FileType: require('file-type'),
+	speed: require('performance-now'),
+    yargs: require('yargs'),
+	process: require('process'),
+	path: require('path'),
+    api: require('api-dylux'),
+	PhoneNumber: require('awesome-phonenumber')
+}
+}
